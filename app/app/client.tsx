26cbff9ff602dc46e8e4c4ff80d1d1c90c75b351
ilawy <mm.ilawy@gmail.com>
2024-04-17 21:12:08 +0200

    improve ui

--- app/app/client.tsx
+++ app/app/client.tsx
@@ -117,7 +117,7 @@ export default function Client({
                 return (
                   <div
                     key={wIndex * 7 + dIndex}
-                    className="day disabled bg-gray-50"
+                    className="day disabled"
                   >
                   </div>
                 );
