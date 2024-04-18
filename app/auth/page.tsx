5d96bdf71dcace157a319607061348376cf123b4
ilawy <mm.ilawy@gmail.com>
2024-04-18 21:07:48 +0200

    random

---
+++ app/auth/page.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+export default async function Page() {
+    return (
+        <>
+        
+           <h1 style={{
+            viewTransitionName: "title",
+          width: "fit-content"
+
+           }}>
+            Sign in
+           </h1>
+        </>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
     <main className="min-h-screen p-3 grid grid-rows-2">
       <div className="flex flex-col justify-evenly items-center pt-24">
         <div className="intro-icon">
-          <div className="circle"></div>
+          <div className="circle" 
+          style={{
+            viewTransitionName: "custom-bg",
+          }}
+          ></div>
           <div className="circle"></div>
         </div>
         <big className="font-extrabold text-3xl">
@@ -16,6 +20,10 @@ export default function Home() {
         </big>
       </div>
       <div className="p-3 flex flex-col items-center justify-evenly">
+        <h1 style={{
+          viewTransitionName: "title",
+          width: "fit-content"
+        }}>Welcome</h1>
         <p className="text-balance">
           Heiver is a set of mental health tools designed to help you stay
           focuesd and shorten the amount of distractions.
